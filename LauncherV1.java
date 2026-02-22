4bd0ba8afea22b74becd235945a5ff96544b85da
leo <leo.scheller@student.englisches-institut.eu>
2026-02-22 14:37:07 +0100

    start in vscode

---
+++ LauncherV1.java
@@ -0,0 +1,8 @@
+public class LauncherV1 {
+
+    public static void main(String[] args){
+        System.out.println("Starting Orbito based on branch (main):");
+        new Spiel();
+        System.out.println("Orbito stopped");
+    }
+}
