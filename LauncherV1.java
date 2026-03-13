86998295e0ace32d73288d96189c96cc9ba2dd1c
DrProb <leo.scheller@student.englisches-institut.eu>
2026-03-13 22:37:17 +0100

    Delete LauncherV1.java
    Die habe ich gebraucht um in VS Code zu Coden, aber da Sie viel lieber mit bluej arbeiten ist das das

--- LauncherV1.java
+++
@@ -1,7 +0,0 @@
-public class LauncherV1 {
-    public static void main(String[] args){
-        System.out.println("Starting Orbito based on branch (main):"); 
-        new Spiel();
-        System.out.println("Orbito stopped");
-    }
-}
